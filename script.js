f58d18b7695d10ee20df97e62300b75d8fd7b778
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-15 17:42:25 -0300

    Adiciona à página um quadro de pixels, com 25 pixels e com style

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-let colorPalette = document.querySelector('#color-palette');
+const colorPalette = document.querySelector('#color-palette');
+const pixelBord = document.querySelector('#pixel-board');
 
-function createForCollors(){
+function createForCollors() {
   for (let index = 0; index < 4; index += 1) {
     let elementPalette = document.createElement('li');
     if (index === 1) {
       elementPalette.style.backgroundColor = 'blue';
-      elementPalette.className = 'color'
+      elementPalette.className = 'color';
       colorPalette.appendChild(elementPalette);
     } else if (index === 2) {
       elementPalette.style.backgroundColor = 'red';
-      elementPalette.className = 'color'
+      elementPalette.className = 'color';
       colorPalette.appendChild(elementPalette);
     } else if (index === 3) {
       elementPalette.style.backgroundColor = 'green';
-      elementPalette.className = 'color'
+      elementPalette.className = 'color';
       colorPalette.appendChild(elementPalette);
     } else {
       elementPalette.style.backgroundColor = 'black';
-      elementPalette.className = 'color'
+      elementPalette.className = 'color';
       colorPalette.appendChild(elementPalette);
     }
-  } 
+  }
 }
 createForCollors();
+
+function createPixels() {
+  for (index = 0; index < 5; index += 1) {
+    let linePixel = document.createElement('div');
+    pixelBord.appendChild(linePixel);
+    for (index2 = 0; index2 < 5; index2 += 1) {
+      let columnPixel = document.createElement('div');
+      columnPixel.className = 'pixel';
+      linePixel.appendChild(columnPixel);
+    }
+  }
+}
+createPixels();
